b3f2735923e48958d238e3e20c86ce3090a5eea0
Thomas F <taforres@asu.edu>
2017-03-04 01:18:12 -0800

    Add handling for basic twitch controls

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from twilio import twiml
 app = Flask(__name__)
 load_dotenv(find_dotenv())
 
+directions = ['forward', 'backward']
+
 @app.route('/message', methods=['POST'])
 def roomba_command():
 	# twilio text message
@@ -17,7 +19,13 @@ def roomba_command():
 	return str(twilio_resp)
 
 def handle_twilio_message(message):
-	return 'Echo: ' + message
+	if message.lower() in directions:
+		return message.lower()
+	try:
+		degree = float(message)
+	except ValueError as e:
+		return 'Invalid command'
+	return str(degree)
 
 if __name__ == '__main__':
 	app.run(debug=True)
