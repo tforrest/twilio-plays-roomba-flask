ba88e6d67f571b1430ab30f233067018f28a2b00
Thomas F <taforres@asu.edu>
2017-03-04 01:05:50 -0800

    Add basic twilio echo server

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv, find_dotenv
+from twilio import twiml
 app = Flask(__name__)
 load_dotenv(find_dotenv())
 
-@app.route('/')
-def hello_world():
-    return jsonify({
-		'hello': 'world'
-	})
+@app.route('/message', methods=['POST'])
+def roomba_command():
+	# twilio text message
+	body = request.form['Body']
+
+	resp = handle_twilio_message(body)
+
+	twilio_resp = twiml.Response()
+	twilio_resp.message(resp)
+	return str(twilio_resp)
+
+def handle_twilio_message(message):
+	return 'Echo: ' + message
 
 if __name__ == '__main__':
 	app.run(debug=True)
